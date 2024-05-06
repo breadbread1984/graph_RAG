cb4dad51840b5f1d54527eca4a8a0b826c285baa
breadbread1984 <breadbread1984@163.com>
2024-05-06 14:36:27 +0800

    update

--- db.py
+++ db.py
@@ -8,20 +8,22 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.text_splitter import SentenceSplitter
 from llama_index.core import SimpleDirectoryReader, KnowledgeGraphIndex, StorageContext, ServiceContext, PromptHelper
 from llama_index.graph_stores.nebula import NebulaGraphStore
+from llama_index.core.retrievers import KnowledgeGraphRAGRetriever
 
 class DocDatabase(object):
   def __init__(self):
     environ['NEBULA_USER'] = 'root'
     environ['NEBULA_PASSWORD'] = 'nebula'
     environ['NEBULA_ADDRESS'] = 'localhost:9669'
-  @staticmethod
-  def load_db():
-    raise NotImplemented
-  @staticmethod
-  def load_doc(doc_dir):
+    graph_store = NebulaGraphStore(space_name = 'llamaindex', edge_types = ['relationship'], rel_prop_names = ['relationship'], tags = ['entity'])
+    self.storage_context = StorageContext.from_defaults(graph_store = graph_store)
+  def load_db(self):
+    graph_rag_retriever = KnowledgeGraphRAGRetriever(storage_context = self.storage_context, verbose = True)
+    query_engine = RetrieverQueryEngine.from_args(graph_rag_retriever)
+    return query_engine
+  def load_doc(self, doc_dir):
     print('load pages of documents')
     documents = SimpleDirectoryReader(doc_dir)
-    graph_store = NebulaGraphStore(space_name = 'llamaindex', edge_types = ['relationship'], rel_prop_names = ['relationship'], tags = ['entity'])
     def messages_to_prompt(message):
       prompt = ''
       for message in messages:
@@ -37,7 +39,6 @@ class DocDatabase(object):
       return prompt
     def completion_to_prompt(completion):
       return f"<|system|>\n</s>\n<|user|>\n{completion}</s>\n<|assistant|>\n"
-    storage_context = StorageContext.from_defaults(graph_store = graph_store)
     service_context = ServiceContext.from_defaults(
       llm = HuggingFaceLLM(
         model_name = 'HuggingFaceH4/zephyr-7b-beta',
@@ -57,7 +58,7 @@ class DocDatabase(object):
     kdb = KnowledgeGraphIndex.from_documents(
       documents,
       max_triples_per_chunk = 10,
-      storage_context = storage_context,
+      storage_context = self.storage_context,
       service_context = service_context,
       space_name = 'llamaindex',
       edge_types = ['relationship'],
@@ -65,7 +66,8 @@ class DocDatabase(object):
       tags = ['entity'],
       include_embeddings = True
     )
-    return kdb
+    query_engine = kdb.as_query_engine(include_text = True, retriever_mode = 'keyword', response_mode = "tree_summarize", embedding = 'hybrid', similarity_top_k = 5)
+    return query_engine
   @staticmethod
   def visualize(kdb, output_html = 'example.html'):
     from pyvis.network import Network
