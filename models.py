7fcf583ba4ed83f5e6a7836b99903cf742d2d6b2
breadbread1984 <breadbread1984@163.com>
2024-05-08 16:11:47 +0800

    update

--- models.py
+++ models.py
@@ -40,14 +40,14 @@ class Llama2(LLM):
     super().__init__()
     login(token = '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')
     self.tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
-    self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', attn_implementation = 'flash_attention_2')
+    self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
     self.model = self.model.to(torch.device(device))
     self.model.eval()
   def _call(self, prompt, stop = None, run_manager = None, **kwargs):
     logits_processor = LogitsProcessorList()
     logits_processor.append(TemperatureLogitsWarper(0.8))
     logits_processor.append(TopPLogitsWarper(0.8))
-    inputs = self.tokenizer(prompt, return_tensors = 'pt')
+    inputs = self.tokenizer.apply_chat_template([{'role': 'user', 'content': prompt}], add_generation_prompt = True, return_tensors = "pt")
     inputs = inputs.to(torch.device(self.model.device))
     outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True)
     input_ids = outputs.sequences
@@ -82,33 +82,4 @@ class Llama3(LLM):
   @property
   def _llm_type(self):
     return "Llama-3-8b-hf"
-
-class ChemDFM(LLM):
-  tokenizer: AutoTokenizer = None
-  model: AutoModelForCausalLM = None
-  def __init__(self, device = 'cuda'):
-    assert device in {'cpu', 'cuda'}
-    super().__init__()
-    login(token = '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')
-    self.tokenizer = AutoTokenizer.from_pretrained('OpenDFM/ChemDFM-13B-v1.0')
-    self.model = AutoModelForCausalLM.from_pretrained('OpenDFM/ChemDFM-13B-v1.0')
-    self.model = self.model.to(torch.device(device))
-    self.model.eval()
-  def _call(self, prompt, stop = None, run_manager = None, **kwargs):
-    logits_processor = LogitsProcessorList()
-    logits_processor.append(TemperatureLogitsWarper(0.9))
-    logits_processor.append(TopKLogitsWarper(20))
-    logits_processor.append(TopPLogitsWarper(0.9))
-    logits_processor.append(RepetitionPenaltyLogitsProcessor(1.05))
-    s = '[Round 0]\nHuman: %s\nAssistant: ' % prompt
-    inputs = self.tokenizer(s, return_tensors = 'pt')
-    inputs = inputs.to(torch.device(self.model.device))
-    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = False, use_cache = True, return_dict_in_generate = True, max_new_tokens = 2048)
-    input_ids = outputs.sequences
-    outputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens = True)
-    response = outputs[0][len(s):]
-    return response
-  @property
-  def _llm_type(self):
-    return "ChemDFM-13b"
 
