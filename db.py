4c168ec7f43cbd617473c2293b36ce17d8f33f5c
breadbread1984 <breadbread1984@163.com>
2024-05-08 16:17:39 +0800

    update

--- db.py
+++ db.py
@@ -3,12 +3,22 @@
 from langchain.graphs import Neo4jGraph
 from langchain.vectorstores.neo4j_vector import Neo4jVector
 from langchain.embedding.huggingface import HuggingFaceEmbedding
+from langchain_experimental.graph_transformers.llm import LLMGraphTransformer
+from models import ChatGLM3, Llama2, Llama3
 
 class DocDatabase(object):
-  def __init__(self, username = 'neo4j', password = None, host = 'localhost'):
+  def __init__(self, username = 'neo4j', password = None, host = 'localhost', model = 'llama3', device = 'cuda'):
     self.username = username
     self.password = password
     self.host = host
+    if model == 'llama2':
+      self.model = Llama2(device = device)
+    elif model == 'llama3':
+      self.model = Llama3(device = device)
+    elif model == 'chatglm3':
+      self.model = ChatGLM3(device = device)
+    else:
+      raise Exception('unknown model!')
   @staticmethod
   def load_db(db_dir):
     vectordb = Neo4jVector.from_existsing_graph(
@@ -40,5 +50,5 @@ class DocDatabase(object):
     split_docs = text_splitter.split_documents(docs)
     # 3) extract triplets from documents
     print('extract triplets from documents')
-    results = 
+    graph = LLMGraphTransformer(llm = self.model).convert_to_graph_documents(docs)
     
