6dac3900c61fb32559d83fe2de160e4978020bc8
breadbread1984 <breadbread1984@163.com>
2024-05-10 16:15:55 +0800

    update

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ def ChatGLM3():
   environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
   return HuggingFaceEndpoint(
     endpoint_url = 'THUDM/chatglm3-6b',
-    task = "text-generation",
+    task = "conversational",
     max_length = 8192,
     do_sample = False,
     top_p =  0.8,
@@ -21,7 +21,7 @@ def Llama2():
   environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
   return HuggingFaceEndpoint(
     endpoint_url = "meta-llama/Llama-2-7b-chat-hf",
-    task = "text-generation",
+    task = "conversational",
     max_length = 4096,
     do_sample = False,
     temperature = 0.8,
@@ -34,11 +34,13 @@ def Llama3():
   environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
   return HuggingFaceEndpoint(
     endpoint_url = "meta-llama/Meta-Llama-3-8B-Instruct",
-    task = "text-generation",
+    task = "conversational",
     max_length = 4096,
     do_sample = False,
     temperature = 0.6,
     top_p = 0.9,
-    eos_token_id = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+    model_kwargs = {
+      "eos_token_id": [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+    }
   )
 
