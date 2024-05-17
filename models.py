2c08142691b751866fcdbf5cea3e9a10d8d3cf04
breadbread1984 <breadbread1984@163.com>
2024-05-17 10:53:01 +0800

    update

--- models.py
+++ models.py
@@ -76,7 +76,7 @@ def Llama3(locally = False):
       task = "text-generation",
       device = 0,
       pipeline_kwargs = {
-        "max_length": 8192,
+        "max_length": 16384,
         "do_sample": False,
         "temperature": 0.6,
         "top_p": 0.9,
