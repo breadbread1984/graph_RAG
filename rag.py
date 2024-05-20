555f9b72c630e2bd22b163fea2d11b29a7b6af97
breadbread1984 <breadbread1984@163.com>
2024-05-20 14:47:43 +0800

    update

--- rag.py
+++ rag.py
@@ -3,8 +3,8 @@
 from db import DocDatabase
 
 class RAG(object):
-  def __init__(self, database = 'neo4j', password = None, doc_dir = None):
-    self.db = DocDatabase(password = password, database = database, locally = True)
+  def __init__(self, host = 'localhost', port = 7687, database = 'neo4j', username = 'neo4j', password = None, doc_dir = None):
+    self.db = DocDatabase(username = username, password = password, host = 'bolt://%s:%d' % (host, port), database = database, locally = True)
     if doc_dir is not None:
       self.db.reset()
       self.db.extract_knowledge_graph(doc_dir)
